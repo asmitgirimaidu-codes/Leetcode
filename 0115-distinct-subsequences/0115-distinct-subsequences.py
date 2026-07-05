44c626531726f0df07487eb86c3b111e9680cc03
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-06 01:33:37 +0530

    Time: 542 ms (44.76%), Space: 69.3 MB (27.69%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numDistinct(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        m, n = len(s), len(t)
+        # Create a DP table with (m+1) x (n+1)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Base case: empty t can be formed by 1 way (deleting all s)
+        for i in range(m + 1):
+            dp[i][0] = 1
+            
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i-1] == t[j-1]:
+                    # Match found: sum of using the char or ignoring it
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    # No match: must ignore s[i-1]
+                    dp[i][j] = dp[i-1][j]
+                    
+        return dp[m][n]
