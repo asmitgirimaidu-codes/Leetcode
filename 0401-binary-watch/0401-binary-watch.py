913493240d821aa38e725009b5029bfc573d536a
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-16 18:29:09 +0530

    Time: 4 ms (20.36%), Space: 12.4 MB (48.6%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        """
+        :type turnedOn: int
+        :rtype: List[str]
+        """
+        ans = []
+
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count('1') + bin(m).count('1') == turnedOn:
+                    ans.append("{}:{:02d}".format(h, m))
+
+        return ans
