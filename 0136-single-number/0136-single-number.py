82c136a0a66d2f5d0cc12ffd3140ada88453991f
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-22 14:59:58 +0530

    Time: 0 ms (100%), Space: 14 MB (28.57%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """:type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
