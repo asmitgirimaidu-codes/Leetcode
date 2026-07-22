bec3ff336a56f8c269468dbbe2583548829e9664
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-22 14:03:25 +0530

    Time: 8 ms (96.88%), Space: 13.8 MB (64.21%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """:type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        m = len(grid)
+        n = len(grid[0])
+
+        # Initialize the first row
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+
+        # Initialize the first column
+        for i in range(1, m):
+            grid[i][0] += grid[i - 1][0]
+
+        # Fill the rest of the grid using dynamic programming
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[m - 1][n - 1]
