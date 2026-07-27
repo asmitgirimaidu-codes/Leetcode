62bcc9680b32083bbcc54445ec938fbcd137b0b5
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-27 10:05:32 +0530

    Time: 0 ms (100%), Space: 29.6 MB (46.61%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        
+        // 1. Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // 2. Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // 3. Reverse the remaining n - k elements
+        reverse(nums.begin() + k, nums.end());
+    }
+};
