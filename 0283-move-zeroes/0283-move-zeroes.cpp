973a6458cc7dc0c0e2469dde8ef71fed80efeded
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-07-27 10:06:40 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.36%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;
+        
+        // Move all non-zero elements to the front of the array
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[lastNonZeroFoundAt], nums[i]);
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+};
