50f884ddac451189cebac42ad989fedcc4534118
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-08-04 10:19:05 +0530

    Time: 4 ms (62.7%), Space: 11.4 MB (66.8%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        std::vector<int> ugly(n);
+        ugly[0] = 1;
+        
+        int i2 = 0, i3 = 0, i5 = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            int next2 = ugly[i2] * 2;
+            int next3 = ugly[i3] * 3;
+            int next5 = ugly[i5] * 5;
+            
+            int next_ugly = std::min({next2, next3, next5});
+            ugly[i] = next_ugly;
+            
+            if (next_ugly == next2) i2++;
+            if (next_ugly == next3) i3++;
+            if (next_ugly == next5) i5++;
+        }
+        
+        return ugly[n - 1];
+    }
+};
