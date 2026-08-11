875d32256f9b20f642fa554e842ad0eccdd0e88d
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-08-11 20:16:58 +0530

    Time: 62 ms (16.34%), Space: 94.7 MB (27.32%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                string pattern = s.substr(0, i);
+                string constructed = "";
+                for (int j = 0; j < n / i; ++j) {
+                    constructed += pattern;
+                }
+                if (constructed == s) return true;
+            }
+        }
+        return false;
+    }
+};
