15f23e9646a5f6ac09b8337f6389bec22db6b808
Asmit Kumar Giri <asmitgirimaidu@gmail.com>
2026-08-24 07:24:22 +0530

    Time: 0 ms (100%), Space: 9.9 MB (87.57%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,44 @@
+#include <queue>
+
+class MyStack {
+private:
+    std::queue<int> q;
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int size = q.size();
+        // Rotate the queue so that the newly added element is at the front
+        for (int i = 0; i < size - 1; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int topElement = q.front();
+        q.pop();
+        return topElement;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
